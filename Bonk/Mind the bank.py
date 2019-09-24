5549a57ff7c0e5534b6ab14426f7a2f2a6967025
Theo Larefalk <theo.larefalk@learnet.se>
2019-09-24 10:32:35 +0200

    Sleep is portant sequel

--- Bonk/Mind the bank.py
+++ Bonk/Mind the bank.py
@@ -5,10 +5,12 @@ userPin = int(input("Skriv in din pinkod: "))
 if pin != userPin:
     exit()
 
+saldo = 0
+
 menu = 0
-# menu 1 instättning
-# menu 2 uttag  
-# menu 3 avsluta
+print ("menu 1 instättning")
+print ("menu 2 uttag")  
+print ("menu 3 avsluta")
 while menu != 3:
     menu = int(input("Skriv in ditt val: "))
     if menu ==1: 

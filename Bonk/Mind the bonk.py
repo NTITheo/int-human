73065f977528f26680d49afa4aed299dc33f9750
Theo Larefalk <theo.larefalk@learnet.se>
2019-10-08 08:48:43 +0200

    florp

--- Bonk/Mind the bonk.py
+++ Bonk/Mind the bonk.py
@@ -4,7 +4,10 @@ pin = 1234
 
 userPin = int(input("Skriv in din pinkod: "))
 
-
+#f = open("saldo.txt", "r")
+#saldo = int(f.read())
+#print (saldo)
+#f.close()
 #saldo = 0.0
 #if pin != userPin:
 #    exit()
@@ -18,14 +21,17 @@ saldo = random.randint (0,100000000)
 menu = 0
 print ("menu 1 instättning")
 print ("menu 2 uttag")  
-print ("menu 3 avsluta")
-while menu != 3:
+print ("menu 3 Balance")
+print ("menu 4 Avsluta")
+while menu != 4:  #Detta är en loop som fungerar som en meny, 
     print("Ditt saldo är: " + str(saldo) + "kr")
-    menu = int(input("Skriv in ditt val[1, 2, 3]: "))
+    menu = int(input("Skriv in ditt val[1, 2, 3, 4]: "))
     if menu == 1: 
         saldo = saldo + float(input("Gör en insättning: "))
     elif menu == 2:
         saldo = saldo - float(input("Gör ett uttag: "))
+    elif menu == 3:
+        print (saldo)
     else:
         print("Fel eller avslut")
 f = open('saldo.txt', 'a')
